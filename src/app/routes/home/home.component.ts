315c95d998e46fcbc42e48518e9f2916d0bc814f
Vanessa Ortega <ortegavan@live.com>
2024-03-22 01:12:01 -0300

    feat: feito template de busca e filtros

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { CategoryFilterComponent } from '../../layout/category-filter/category-filter.component';
 import { HeaderComponent } from '../../ui/header/header.component';
+import { CheckboxModule } from 'primeng/checkbox';
+import { InputTextModule } from 'primeng/inputtext';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { DividerModule } from 'primeng/divider';
+import { ToggleButtonModule } from 'primeng/togglebutton';
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [CategoryFilterComponent, HeaderComponent],
+    imports: [
+        CommonModule,
+        CategoryFilterComponent,
+        HeaderComponent,
+        CheckboxModule,
+        InputTextModule,
+        IconFieldModule,
+        InputIconModule,
+        DividerModule,
+        ToggleButtonModule,
+    ],
 })
-export class HomeComponent {}
+export class HomeComponent {
+    categories = [
+        'Carnes',
+        'Aves',
+        'Peixes e frutos do mar',
+        'Vegetarianos',
+        'Saladas',
+        'Massas',
+        'Sobremesas',
+    ];
+
+    tags = ['Rápido', 'Saudável', 'Fácil', 'Sem glúten', 'Sem lactose'];
+}
