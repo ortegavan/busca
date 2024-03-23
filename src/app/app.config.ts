d0464361480861534960a4f08af0eca21b27dfc1
Vanessa Ortega <ortegavan@live.com>
2024-03-23 14:58:44 -0300

    feat: implementada paginação

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideRouter(routes), provideHttpClient()],
+    providers: [
+        provideRouter(routes),
+        provideHttpClient(),
+        importProvidersFrom(BrowserAnimationsModule),
+    ],
 };
--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -8,7 +8,8 @@ import { IconFieldModule } from 'primeng/iconfield';
 import { InputIconModule } from 'primeng/inputicon';
 import { DividerModule } from 'primeng/divider';
 import { ToggleButtonModule } from 'primeng/togglebutton';
-import { RatingModule } from 'primeng/rating';
+import { PaginatorModule } from 'primeng/paginator';
+import { DropdownModule } from 'primeng/dropdown';
 import { CategoryService } from '../../services/category.service';
 import { TagService } from '../../services/tag.service';
 import { RecipeService } from '../../services/recipe.service';
@@ -16,6 +17,13 @@ import { Observable } from 'rxjs';
 import { Category } from '../../models/category.model';
 import { Recipe } from '../../models/recipe.model';
 import { Tag } from '../../models/tag.model';
+
+type PageEvent = {
+    first?: number;
+    page?: number;
+    pageCount?: number;
+    rows?: number;
+};
 
 @Component({
     selector: 'app-home',
@@ -32,10 +40,14 @@ import { Tag } from '../../models/tag.model';
         InputIconModule,
         DividerModule,
         ToggleButtonModule,
-        RatingModule,
+        PaginatorModule,
+        DropdownModule,
     ],
 })
 export class HomeComponent implements OnInit {
+    first: number = 0;
+    rows: number = 9;
+
     categoryService = inject(CategoryService);
     tagService = inject(TagService);
     recipeService = inject(RecipeService);
@@ -48,5 +60,10 @@ export class HomeComponent implements OnInit {
         this.categories$ = this.categoryService.getCategories();
         this.tags$ = this.tagService.getTags();
         this.recipes$ = this.recipeService.getRecipes();
+    }
+
+    onPageChange(event: PageEvent) {
+        this.first = event.first || 0;
+        this.rows = event.rows || 9;
     }
 }
